b6fc4ee00dd57f1b00c89723c1c257d9e070c802
Jaxson Kevitt <jaxkevitt@gmail.com>
2024-08-07 15:25:34 -0400

    setup gh-pages

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const footer = () => {
+  return (
+    <div className='footer'>
+    <p> Copyright @ 2024, CryptoInsights - All Rights Reserved.</p>
+    </div>
+  )
+}
+
+export default footer
